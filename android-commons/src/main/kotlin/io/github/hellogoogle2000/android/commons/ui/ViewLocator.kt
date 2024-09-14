6769ba28db10c94c1ee62610f3f26e105aa61031
easing <easing@onyx-international.com>
2024-09-14 17:06:53 +0800

    init project

--- android-commons/src/main/kotlin/io/github/hellogoogle2000/android/commons/ui/ViewLocator.kt
+++ android-commons/src/main/kotlin/io/github/hellogoogle2000/android/commons/ui/ViewLocator.kt
@@ -1,9 +1,40 @@
 package io.github.hellogoogle2000.android.commons.ui
 
 import android.graphics.Rect
+import android.util.Size
 import android.view.View
+import io.github.hellogoogle2000.android.commons.context.ActivityEx.getFrontActivity
+import io.github.hellogoogle2000.android.commons.context.ActivityEx.getTopContentView
 
 object ViewLocator {
+
+    fun getWindowSize(): Size {
+        val activity = getFrontActivity()!!
+        val metrics = activity.windowManager.maximumWindowMetrics
+        metrics.windowInsets
+        return Size(0, 0)
+    }
+
+    fun getApplicationSize(): Size {
+        TODO()
+    }
+
+    fun getScreenSize(): Size {
+        TODO()
+    }
+
+    fun View.getWindowOffset(): Location {
+        val anotherView = context.getTopContentView()
+        return getWindowOffset(anotherView)
+    }
+
+    fun View.getWindowOffset(anotherView: View): Location {
+        val thisLocation = rootView.locationOnScreen()
+        val anotherLocation = anotherView.locationOnScreen()
+        val dx = thisLocation.x - anotherLocation.x
+        val dy = thisLocation.y - anotherLocation.y
+        return Location(dx = dx, dy = dy)
+    }
 
     fun View.locationInParent(): Location {
         return Location(left, top)
@@ -13,6 +44,10 @@ object ViewLocator {
         val out = IntArray(2)
         getLocationInWindow(out)
         return Location(out[0], out[1])
+    }
+
+    fun View.locationInApplication(): Location {
+        TODO()
     }
 
     fun View.locationOnScreen(): Location {
