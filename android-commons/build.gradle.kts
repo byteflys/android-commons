8902b2df8f4b8ddba5ad287ba45b082b14e2f22c
easingforever2000@gmail.com <easingforever2000@gmail.com>
2024-11-10 10:55:28 +0800

    android commons

--- android-commons/build.gradle.kts
+++ android-commons/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
     id("com.android.library")
     id("signing")
     id("com.vanniktech.maven.publish")
@@ -34,39 +35,39 @@ android {
 dependencies {
 
     // Commons Serial
-    api("io.github.hellogoogle2000:kotlin-commons:1.0.17")
+    api("io.github.hellogoogle2000:kotlin-commons:1.0.18")
 
-    // Kotlin
-    api("androidx.core:core-ktx:1.13.1")
-
-    // Android Adapter
-    api("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
+    // Android Adapters
+    api("androidx.core:core-ktx:1.15.0")
+    api("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
     api("io.reactivex.rxjava3:rxandroid:3.0.2")
 
     // AndroidX
     api("androidx.appcompat:appcompat:1.7.0")
-    api("androidx.activity:activity:1.7.0")
+    api("androidx.activity:activity:1.9.3")
     api("androidx.fragment:fragment-ktx:1.8.2")
     api("androidx.constraintlayout:constraintlayout:2.1.4")
     api("com.google.android.material:material:1.12.0")
 
-    // Jetpack
+    // Navigation
     api("androidx.navigation:navigation-ui-ktx:2.8.0")
     api("androidx.navigation:navigation-fragment-ktx:2.8.0")
 
     // Lifecycle
-    api("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
-    api("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.4")
+    api("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
+    api("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
 
     // Compose
-    api("androidx.compose.ui:ui:1.7.1")
-    api("androidx.compose.ui:ui-graphics:1.7.1")
-    api("androidx.compose.ui:ui-tooling-preview:1.7.1")
+    api("androidx.compose.ui:ui:1.7.5")
+    api("androidx.compose.ui:ui-graphics:1.7.5")
+    api("androidx.compose.ui:ui-tooling:1.7.5")
+    api("androidx.compose.ui:ui-tooling-preview:1.7.5")
+    api("androidx.compose.ui:ui-tooling-preview-android:1.7.5")
     api("androidx.compose.material3:material3:1.3.0")
     api("androidx.activity:activity-compose:1.9.2")
-    api(platform("androidx.compose:compose-bom:2024.09.01"))
+    api("io.coil-kt.coil3:coil-compose:3.0.2")
 
-    // Android Utils
+    // Third-Part
     api("com.blankj:utilcodex:1.31.0")
 }
 
